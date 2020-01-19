4b4455200e38e8a0af974fb8c88ef84473f529d5
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-01-19 12:48:17 -0300

    Criado projeto

---
+++ Unike/Unike/View/HomeView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Unike.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomeView : ContentPage
+	{
+		public HomeView ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnAccess(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginView());
+        }
+
+        private void btnCreate(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CadastroContatoView());
+        }
+    }
+}
